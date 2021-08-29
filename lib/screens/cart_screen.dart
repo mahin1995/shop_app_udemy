b33894277b04a0f9b64012c2ed53051c0bc55b81
Mohaimanul islam Mahin <56087765+Mahin14@users.noreply.github.com>
2021-08-29 17:43:51 +0600

    from offcie pc

---
+++ lib/screens/cart_screen.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/cart.dart';
+
+class CartScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final cart = Provider.of<Cart>(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("your cart"),
+      ),
+      body: Column(
+        children: [
+          Card(
+            margin: EdgeInsets.all(15),
+            child: Padding(
+              padding: EdgeInsets.all(8),
+              child: Row(
+                children: [
+                  Text(
+                    'Total',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Chip(
+                    label: Text('\$${cart.totalAmount}'),
+                    backgroundColor: Theme.of(context).primaryColor,
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
