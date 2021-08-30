5ac4d7fe0bfdf811026969e128c3d31e83c468db
mahin1995 <mohimanul8@gmail.com>
2021-08-30 21:23:43 +0600

    from home pc

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -10,11 +10,36 @@ class ProductDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final productId = ModalRoute.of(context)!.settings.arguments as String;
-    final loadedProducts = Provider.of<Products>(context, listen: false).findById(productId);
+    final loadedProducts =
+        Provider.of<Products>(context, listen: false).findById(productId);
 
     return Scaffold(
       appBar: AppBar(
         title: Text(loadedProducts.title),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.all(10),
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                loadedProducts.imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              '\$${loadedProducts.price}',
+              style: TextStyle(color: Colors.grey, fontSize: 20),
+            ),
+            SizedBox(height: 10,),
+            Text(loadedProducts.description,textAlign: TextAlign.center,softWrap: true,)
+          ],
+        ),
       ),
     );
   }
