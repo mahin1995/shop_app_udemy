affe68e95f4053cb24ecc4456fab570ed841776b
Mohaimanul islam Mahin <56087765+Mahin14@users.noreply.github.com>
2021-09-06 16:05:50 +0600

    from office

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -5,38 +5,35 @@ import './product.dart';
 
 class Products with ChangeNotifier {
   List<Product_m> _item = [
-    Product_m(
-      id: 'p1',
-      title: 'Red Shirt',
-      description: 'A red shirt - it is pretty red!',
-      price: 29.99,
-      imageUrl:
-          'https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_1280.jpg',
-    ),
-    Product_m(
-      id: 'p2',
-      title: 'Trousers',
-      description: 'A nice pair of trousers.',
-      price: 59.99,
-      imageUrl:
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Trousers%2C_dress_%28AM_1960.022-8%29.jpg/512px-Trousers%2C_dress_%28AM_1960.022-8%29.jpg',
-    ),
-    Product_m(
-      id: 'p3',
-      title: 'Yellow Scarf',
-      description: 'Warm and cozy - exactly what you need for the winter.',
-      price: 19.99,
-      imageUrl:
-          'https://live.staticflickr.com/4043/4438260868_cc79b3369d_z.jpg',
-    ),
-    Product_m(
-      id: 'p4',
-      title: 'A Pan',
-      description: 'Prepare any meal you want.',
-      price: 49.99,
-      imageUrl:
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Cast-Iron-Pan.jpg/1024px-Cast-Iron-Pan.jpg',
-    ),
+    // Product_m(
+    //   id: 'p1',
+    //   title: 'Red Shirt',
+    //   description: 'A red shirt - it is pretty red!',
+    //   price: 29.99,
+    //   imageUrl: 'https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_1280.jpg',
+    // ),
+    // Product_m(
+    //   id: 'p2',
+    //   title: 'Trousers',
+    //   description: 'A nice pair of trousers.',
+    //   price: 59.99,
+    //   imageUrl:
+    //       'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Trousers%2C_dress_%28AM_1960.022-8%29.jpg/512px-Trousers%2C_dress_%28AM_1960.022-8%29.jpg',
+    // ),
+    // Product_m(
+    //   id: 'p3',
+    //   title: 'Yellow Scarf',
+    //   description: 'Warm and cozy - exactly what you need for the winter.',
+    //   price: 19.99,
+    //   imageUrl: 'https://live.staticflickr.com/4043/4438260868_cc79b3369d_z.jpg',
+    // ),
+    // Product_m(
+    //   id: 'p4',
+    //   title: 'A Pan',
+    //   description: 'Prepare any meal you want.',
+    //   price: 49.99,
+    //   imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Cast-Iron-Pan.jpg/1024px-Cast-Iron-Pan.jpg',
+    // ),
   ];
 
   var _showFavoritesOnly = false;
@@ -58,30 +55,55 @@ class Products with ChangeNotifier {
     );
   }
 
+
+
+  Future<void> fetchAndSetProducts() async {
+    final url = Uri.parse("https://flutter-first-27064-default-rtdb.firebaseio.com/product.json");
+    try {
+      final response = await http.get(url);
+      Map<String, dynamic> extractedData = jsonDecode(response.body);
+      // final extractedData = json.decode(response.body) as Map<String, dynamic>;
+      final List<Product_m> loadedProducts = [];
+      extractedData.forEach((prodId, prodData) {
+        loadedProducts.add(Product_m(
+          id: prodId,
+          title: prodData['title'],
+          description: prodData['description'],
+          price: prodData['price'],
+          isFavorite: true,
+          imageUrl: prodData['imageUrl'],
+        ));
+      });
+      _item = loadedProducts;
+      notifyListeners();
+    } catch (error) {
+      throw (error);
+    }
+  }
+
   Future<void> addProduct(Product_m product) async {
-    final url = Uri.parse(
-        "https://flutter-first-27064-default-rtdb.firebaseio.com/product.json");
-        try{
-    final response = await http.post(url,
-        body: jsonEncode({
-          'title': product.title,
-          'description': product.description,
-          'price': product.price,
-          'imageUrl': product.imageUrl
-        }));
-    print(jsonDecode(response.body));
-    final newProduct = Product_m(
-        title: product.title,
-        price: product.price,
-        imageUrl: product.imageUrl,
-        id: jsonDecode(response.body)["name"],
-        description: product.description);
-    _item.add(newProduct);
-    notifyListeners();
- }catch(error){
-   print(error.toString());
-   throw error;
- }   // _item.insert(0, newProduct);///to add to first of list
+    final url = Uri.parse("https://flutter-first-27064-default-rtdb.firebaseio.com/product.json");
+    try {
+      final response = await http.post(url,
+          body: jsonEncode({
+            'title': product.title,
+            'description': product.description,
+            'price': product.price,
+            'imageUrl': product.imageUrl
+          }));
+      print(jsonDecode(response.body));
+      final newProduct = Product_m(
+          title: product.title,
+          price: product.price,
+          imageUrl: product.imageUrl,
+          id: jsonDecode(response.body)["name"],
+          description: product.description);
+      _item.add(newProduct);
+      notifyListeners();
+    } catch (error) {
+      print(error.toString());
+      throw error;
+    } // _item.insert(0, newProduct);///to add to first of list
   }
 
   // void showFavoritesOnly() {
--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -5,6 +5,7 @@ import '../screens/cart_screen.dart';
 import '../widgets/badge.dart';
 import '../widgets/products_grid.dart';
 import '../providers/cart.dart';
+import '../providers/products.dart';
 
 // import 'package:provider/provider.dart';
 // import '../providers/products.dart';
@@ -20,6 +21,34 @@ class ProductsOverviewScreen extends StatefulWidget {
 
 class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
   var _showOnlyFavourite = false;
+  var _isInit = false;
+  var _isLoading = false;
+  @override
+  void initState() {
+    _isInit = true;
+
+    // Provider.of<Products>(context,listen: false).fetchaAndSetProduct(); //if listen false it can be use in init state
+    // Future.delayed(Duration.zero).then((value) {
+    //   Provider.of<Products>(context).fetchaAndSetProduct();
+    // });
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    if (_isInit) {
+      setState(() {
+        _isLoading = true;
+      });
+      Provider.of<Products>(context).fetchAndSetProducts().then((value) {
+        setState(() {
+          _isLoading = false;
+        });
+      });
+    }
+    _isInit = false;
+    super.didChangeDependencies();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -66,9 +95,13 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
         ],
       ),
       drawer: AppDrwar(),
-      body: ProductGrid(_showOnlyFavourite),
+      body: _isLoading
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : ProductGrid(_showOnlyFavourite),
     );
-  } 
+  }
 }
 
 
