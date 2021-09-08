c4212a0452ada46a922c8c7d8e7c66786ad6b108
mahin1995 <mohimanul8@gmail.com>
2021-09-09 01:07:20 +0600

    from hme

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -4,6 +4,7 @@ import 'package:flutter/cupertino.dart';
 
 import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
+import 'package:shop_app_udemy/models/http_exception.dart';
 
 
 class Auth with ChangeNotifier{
@@ -15,10 +16,20 @@ class Auth with ChangeNotifier{
 
 Future <void> _authneticate(String email ,String password,String urlSeagment) async{
   final url =Uri.parse('https://identitytoolkit.googleapis.com/v1/accounts:$urlSeagment?key=AIzaSyCSd6ffxtMI7uqU8yrIzwN80H5GQlVjZUA');
-    final response= await http.post(url,body: json.encode({
+  try{
+  final response= await http.post(url,body: json.encode({
       'email':email,'password':password,'returnSecureToken':true
     }));
     print(json.decode(response.body));
+    final responseData=json.decode(response.body);
+    if(responseData['error'] != null){
+      throw HttpException(responseData['error']['message']);
+    }
+  }
+  catch(error){
+throw error;
+  }
+  
   }
 
 
